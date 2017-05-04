59f1bbfb88d70af0c26321c8a6b094764676fa1c
Puck Treeratpituk <pucktada@winterfell.local>
2017-05-04 14:16:53 +0700

    add some initial files

---
+++ char_dictionary.py
@@ -0,0 +1,135 @@
+import numpy as np
+
+class CharDictionary:
+    
+    def __init__(self):
+        thai_chars = 'กขฃคฅฆงจฉชซฌญฐฎฏฐฑฒณดตถทธนบปผฝพฟภมยรฤลฦวศษสหฬอฮฯะัาำิีึืุูเแโใไๅๆ็่้๊๋์ํ' # 1..72
+        numbers = '๐๑๒๓๔๕๖๗๘๙0123456789' # => 73 ' '=> 74, '.'=>75
+        symbols = '()[]<>{}:;^+-*/_=#!~%\\/\'"`?' # 76
+        eng_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' # 77
+
+        self._class_for = {'B':1, 'M':2, 'E':3, 'S':4}
+
+        self._cid2char = ['', '_'] # ''=padding, _=_UNK
+        self._char2cid = dict()
+        
+        next_idx = 2
+        # 1 ... len(thai_chars)
+        for c in thai_chars:
+            self._char2cid[c] = next_idx
+            self._cid2char.append(c)
+            next_idx += 1
+                    
+        last_char_id = len(thai_chars) + 1
+
+        for d in numbers:
+            self._char2cid[d] = last_char_id + 1
+        self._cid2char.append('$')
+        
+        self._char2cid[' '] = last_char_id + 2
+        self._cid2char.append(' ')
+        
+        self._char2cid['.'] = last_char_id + 3
+        self._cid2char.append('.')
+        
+        for s in symbols:
+            self._char2cid[s] = last_char_id + 4
+        self._cid2char.append('#')  
+
+        for s in eng_chars:
+            self._char2cid[s] = last_char_id + 5
+        self._cid2char.append('A')
+
+    def num_char_classes(self):
+        ''' return the number of character classes '''
+        # minus 1... cause cid 0 is not used... reserved for padding
+        return len(self._cid2char) - 1 # 
+    
+    def num_label_classes(self):
+        ''' return the number of label classes '''
+        return len(self._class_for) # B, M, E, S
+
+    def words2cids(self, words):
+        chars, labels = self.words2chars(words)
+        cids = self.chars2cids(chars)
+        return cids, labels
+        
+    # labels = {B, M, E, S} <=== 0:begin, 1:middle, 2:end, 3:single
+    # convert words array to character array, and create a labels
+    def words2chars(self, words):
+        """
+        convert array of words, into array of characters and array of word boundaries
+        """
+        chars, labels = [], []
+
+        for w in words:
+            if len(w) == 1: # single char
+                chars.append(w[0])
+                labels.append(self._class_for['S']) # 'S'
+            elif len(w) > 0:
+                chars.append(w[0])
+                labels.append(self._class_for['B']) # 'B'
+                for i in range(len(w)-2):
+                    chars.append(w[i+1])
+                    labels.append(self._class_for['M']) # 'M'
+                chars.append(w[-1])
+                labels.append(self._class_for['E']) # 'E'
+        return chars, labels
+
+    def chars2words(self, chars, labels):
+        """
+        convert array of characters and array of word boundaries into array of words
+        """
+        words = []
+        word = ''
+        class_for = {'B':1, 'M':2, 'E':3, 'S':4}
+        for i in range(len(chars)):
+            c = chars[i] # reverse_dict[data[i]]
+            l = labels[i] # 0:begin, 1:middle, 2:end, 3:single
+            if l == self._class_for['B']: # begin
+                if len(word) != 0: # somehow, still have left-over word
+                    words += [word]
+                word = c
+            elif l == self._class_for['M']: # middle
+                word += c
+            elif l == self._class_for['E']: # end
+                word += c
+                words += [word]
+                word = ''
+            elif l == self._class_for['S']: # single
+                if (len(word) != 0):
+                    words += [word]
+                words += [c]
+                word = ''
+        if (len(word) != 0):
+            words += [word]
+        return words
+    
+    def cidOf(self, c):
+        if c in self._char2cid:
+            return self._char2cid[c]
+        else:
+            return 1 # UNK_ID
+    
+    def chars2cids(self, chars):
+        return [self.cidOf(c) for c in chars]
+        
+    def cids2chars(self, cids):
+        return [self._cid2char[i] for i in cids]
+
+def main():
+    words = ['กฎหมาย', 'กับ', '1', 'การ', 'เบียดบัง', 'คน']
+    
+    dic = CharDictionary()
+    chars, labels = dic.words2chars(words)
+    cids = dic.chars2cids(chars)
+    
+    #print(chars)
+    #print(dic.chars2cids(chars))
+    #print(dic.cids2chars(cids))
+    
+    #words2 = dic.chars2words(chars, labels)
+    #print(words2)
+
+if __name__ == '__main__':
+    main()
